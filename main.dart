418b3886891c3db4e8115b02c176819b5f890b94
SaulFernand0 <saulolivas@upeu.edu.pe>
2021-10-21 22:25:40 -0500

    v1.0

---
+++ main.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+import 'Libro.dart';
+
+main() {
+  Libro libro = Libro();
+  print("Ingrese el id del libro:");
+  var id = stdin.readLineSync();
+  libro.id = id;
+  print("Ingrese el título del libro:");
+  var title = stdin.readLineSync();
+  libro.titulo = title;
+  print("Ingrese el nombre del autor:");
+  var nompe = stdin.readLineSync();
+  libro.nombres_persona = nompe;
+  print("Ingrese el apellido del autor:");
+  var apeper = stdin.readLineSync();
+  libro.apellidos_persona = apeper;
+  print("Ingrese el nro de paginas:");
+  var pag = stdin.readLineSync();
+  libro.paginas = pag;
+  print("Ingrese la ISBN del libro:");
+  var edi = stdin.readLineSync();
+  libro.edicion = edi;
+  print("Ingrese la editorial del libro:");
+  var edit = stdin.readLineSync();
+  libro.editorial = edit;
+  print("Ingrese el dia del lanzamiento del libro : ");
+  var d = stdin.readLineSync();
+  libro.day = d;
+  print("Ingrese el mes del lanzamiento del libro : ");
+  var m = stdin.readLineSync();
+  libro.month = m;
+  print("Ingrese el año del lanzamiento del Libro : ");
+  var y = stdin.readLineSync();
+  libro.year = y;
+
+  libro.imprimir();
+}
